38985802f85e4f3cf296009f0f534b329510e026
Akyla <akylaaquino@hotmail.com>
2023-02-20 19:32:36 -0300

    feat: Criação tabela da home e input

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,22 +2,17 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 export interface PeriodicElement {
-  name: string;
+  valor: string;
   position: number;
   data: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 2, name: 'Helium', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 3, name: 'Lithium', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 4, name: 'Beryllium', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 5, name: 'Boron', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 6, name: 'Carbon', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 7, name: 'Nitrogen', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 8, name: 'Oxygen', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 9, name: 'Fluorine', data: new Date(Date.now()).toLocaleDateString() },
-  { position: 10, name: 'Neon', data: new Date(Date.now()).toLocaleDateString() },
+  {
+    position: 1,
+    valor: 'Hydrogen',
+    data: new Date(Date.now()).toLocaleDateString(),
+  },
 ];
 @Component({
   selector: 'app-home',
@@ -26,7 +21,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class HomeComponent {
   constructor(private activatedRoute: ActivatedRoute) {}
-  displayedColumns: string[] = ['position', 'name', 'data'];
+  displayedColumns: string[] = ['position', 'valor', 'data'];
   dataSource = ELEMENT_DATA;
   getValue() {
     return this.activatedRoute.snapshot.paramMap.get('name');
